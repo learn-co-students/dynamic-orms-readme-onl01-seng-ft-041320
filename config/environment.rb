c83b13bbf43ed534942558ab94f699be5554a065
dannypadron09 <danny.padron09@gmail.com>
2020-05-16 13:34:05 -0400

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -14,3 +14,12 @@ SQL
 
 DB[:conn].execute(sql)
 DB[:conn].results_as_hash = true
+
+### Here we are doing :
+  ### 1. Creating the database
+  ### 2. Drop ~songs~ to avoid an error
+  ### 3. Creating the ~songs~ table
+
+## Use the ~#results_as_has~ metho, from SQLite3-Ruby gem.
+## This method says when a ~SELECT~ statement is executed don't return a database row
+## as an array, return it as a hash with the column names as keyes.
